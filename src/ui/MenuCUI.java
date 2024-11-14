3f3abe6f76854992f30448f00d55dd8e5c28170e
CGM <mrmouse.guru@gmail.com>
2024-11-14 10:57:53 +0700

    update sourrce code v3

--- src/ui/MenuCUI.java
+++ src/ui/MenuCUI.java
@@ -9,6 +9,8 @@ public class MenuCUI {
 	//fields
 	private PrintWriter screenOutput = null;
 	private Scanner keyBoardInput = null;
+	private String prompt;
+	private String command;
 	//functions - methods
 	
 	//constructor
@@ -20,6 +22,8 @@ public class MenuCUI {
 	public MenuCUI(PrintWriter _screenOutput, Scanner _keyBoardInput) {
 		screenOutput = _screenOutput;
 		keyBoardInput = _keyBoardInput;
+		prompt = "->";
+		command = " ";
 	}
 	
 	
@@ -28,6 +32,17 @@ public class MenuCUI {
 		// vẽ lời nhắc/ hướng dẫn cho người dùng
 		screenOutput.println("go lenh \"help\" de duoc ho tro!");
 		screenOutput.flush();
+		
+		
+		//vẽ liên tục lên màn hình
+		//phải dùng vòng lặp
+		// for
+		//while
+		//do ... while
+		while(true) {
+			screenOutput.print(prompt);screenOutput.flush();
+			command = keyBoardInput.nextLine();//"help"
+		}
 
 	}
 	
