e0176b15c44252ed692cfb6d402bd5054892cdb9
CGM <mrmouse.guru@gmail.com>
2024-11-21 11:26:10 +0700

    update source code buoi11 v2

---
+++ src/entity/SinhVienKT.java
@@ -0,0 +1,28 @@
+package entity;
+
+import java.util.Date;
+
+public class SinhVienKT extends SinhVien{
+	private double diemMarketing;
+	private double diemSales;
+	
+public SinhVienKT() {
+		
+	}
+	
+	public SinhVienKT(String _hoTen, String _diaChi, Date _ngaySinh,
+			double _diemMarketing, double _diemSales) {
+		
+		super(_hoTen, "KT", _diaChi, _ngaySinh);
+		
+		diemMarketing = _diemMarketing;
+		diemSales = _diemSales;
+	}
+	
+	@Override
+	protected double tinhDiemTB() {
+		// TODO Auto-generated method stub
+		return (2 * diemMarketing + diemSales) / 3;
+	}
+
+}
--- src/ui/ThongTinSVCUI.java
+++ src/ui/ThongTinSVCUI.java
@@ -1,7 +1,13 @@
 package ui;
 
 import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
+
+import entity.SinhVienKT;
+import entity.SinhVienKTPM;
 
 public class ThongTinSVCUI {
 	
@@ -11,6 +17,7 @@ public class ThongTinSVCUI {
 	private String hoTenPrompt, nganhPrompt, ngaySinhPrompt, 
 	diaChiPrompt, diemJavaPrompt, diemCssPrompt, diemHtmlPrompt,
 	diemMarketingPrompt, diemSalesPrompt;
+	private SimpleDateFormat simpleFormat = null;
 	//functions - methods
 	public ThongTinSVCUI() {
 		
@@ -21,20 +28,61 @@ public class ThongTinSVCUI {
 		screenOutput = _screenOutput;
 		keyBoardInput = _keyBoardInput;
 		hoTenPrompt = "NHAP HO TEN SINH VIEN: ";
-		nganhPrompt = "NHAP CHUYEN NGANH: ";
-		ngaySinhPrompt = "NHAP NGAY SINH CUA SINH VIEN: ";
+		nganhPrompt = "NHAP CHUYEN NGANH [\"KTPM\" / \"KT\"]: ";
+		ngaySinhPrompt = "NHAP NGAY SINH CUA SINH VIEN (dd/MM/yyyy): ";
 		diaChiPrompt = "NHAP DIA CHI NOI O CUA SINH VIEN: ";
 		diemJavaPrompt = "NHAP DIEM CUA MON JAVA: ";
 		diemCssPrompt = "NHAP DIEM CUA MON CSS: ";
 		diemHtmlPrompt = "NHAP DIEM CUA MON HTML: ";
 		diemMarketingPrompt = "NHAP DIEM MON MARKETING: ";
 		diemSalesPrompt = "NHAP DIEM MON SAELS: ";
+		simpleFormat = new SimpleDateFormat("dd/MM/yyyy");
 	}
 	
 	public void nhapThongTinSV() {
 		screenOutput.print(hoTenPrompt);screenOutput.flush();
 		String hoTen = keyBoardInput.nextLine();
 		//sv code tiếp ...
+		screenOutput.print(nganhPrompt);screenOutput.flush();
+		String nganh = keyBoardInput.nextLine();
+		screenOutput.print(diaChiPrompt);screenOutput.flush();
+		String diaChi = keyBoardInput.nextLine();
+		
+		screenOutput.print(ngaySinhPrompt);screenOutput.flush();
+		String ngaySinh = keyBoardInput.nextLine();//"dd/MM/yyyy"
+		//chuyển String thành java.util.Date
+		Date ngaySinhJava = null;
+		try {
+			ngaySinhJava =  simpleFormat.parse(ngaySinh);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//diem
+		if("KTPM".equalsIgnoreCase(nganh)) {
+			screenOutput.print(diemJavaPrompt);screenOutput.flush();
+			double diemJava = keyBoardInput.nextDouble();
+			screenOutput.print(diemCssPrompt);screenOutput.flush();
+			double diemCss = keyBoardInput.nextDouble();
+			screenOutput.print(diemHtmlPrompt);screenOutput.flush();
+			double diemHtml = keyBoardInput.nextDouble();
+			
+			//đối tượng sinh viên phần mềm
+			SinhVienKTPM svpm = new SinhVienKTPM(hoTen, diaChi, ngaySinhJava, 
+					diemJava, diemCss, diemHtml);
+		}
+		
+		if("KT".equalsIgnoreCase(nganh)) {
+			screenOutput.print(diemMarketingPrompt);screenOutput.flush();
+			double diemMarketing = keyBoardInput.nextDouble();
+			screenOutput.print(diemSalesPrompt);screenOutput.flush();
+			double diemSales = keyBoardInput.nextDouble();
+			
+			//đối tượng sinh viên kinh tế
+			SinhVienKT svkt = new SinhVienKT(hoTen, diaChi, ngaySinhJava, 
+					diemMarketing, diemSales);
+		}
 	}
 
 }
